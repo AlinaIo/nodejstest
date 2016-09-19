ab721dd64e268178fc83b4d117eb1f4924baea23
Alina <alinaionescu85@gmail.com>
2016-09-19 21:05:48 +0300

    added unit test- not working

--- getRequest.js
+++ getRequest.js
@@ -1,5 +1,5 @@
-var request = require("request");
-
-request("http://127.0.0.1:8081/", function(error, response, body) {
-    console.log(body);
-});
+// var request = require("request");
+//
+// request("http://127.0.0.1:8081/", function(error, response, body) {
+//     console.log(body);
+// });
