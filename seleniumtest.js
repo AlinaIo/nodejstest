011d21674d74f67d264796d7a5a3a13ba740018f
Alina <alinaionescu85@gmail.com>
2016-09-20 23:36:56 +0300

    selenium without asserts

--- seleniumtest.js
+++ seleniumtest.js
@@ -1,16 +1,22 @@
 /**
  * Created by tdt on 20.09.2016.
  */
-var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
+var webdriver = require('selenium-webdriver');
 
 var driver = new webdriver.Builder()
-    .forBrowser('firefox')
+    .forBrowser('chrome')
     .build();
 
-driver.get('http://www.google.com/ncr');
-driver.findElement(By.name('q')).sendKeys('webdriver');
-driver.findElement(By.name('btnG')).click();
-driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+var name = "test";
+var email = "tdt@tdt.tdt";
+var description = "This is a tdt test.";
+
+driver.get('http://127.0.0.1:1185');
+driver.findElement(webdriver.By.id('name')).sendKeys(name);
+driver.findElement(webdriver.By.id('email')).sendKeys(email);
+driver.findElement(webdriver.By.id('description')).sendKeys(description);
+driver.findElement(webdriver.By.xpath('/html/body/form/fieldset/input[3]')).click();
+driver.wait(function() {
+    return driver.isElementPresent(webdriver.By.xpath('/html/body/pre'));
+}, 3000);
 driver.quit();
